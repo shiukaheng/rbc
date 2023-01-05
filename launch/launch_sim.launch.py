43850e571f67fb10f38f7f9ca7e39fe2d292ab1b
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-01-05 22:28:06 +0800

    formatting

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,8 +1,6 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -12,7 +10,6 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
