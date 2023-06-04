f05992286eb16784fea92539c834c34207c486c2
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-06-04 02:22:55 +0100

    compiles but broken

--- ros/robocock/src/control/omniwheel_base_controller/omniwheel_base_controller.cpp
+++ ros/robocock/src/control/omniwheel_base_controller/omniwheel_base_controller.cpp
@@ -5,12 +5,19 @@
 // #include <realtime_tools/realtime_publisher.h> - will be used later for odometry
 #include <geometry_msgs/Twist.h>
 #include <ros/ros.h>
+#include "./omniwheel_kinematics.h"
 
 // TODO: Implement cmd_vel and odometry, but remember to use realtime_tools
 
 class OmniwheelBaseController : public controller_interface::Controller<hardware_interface::VelocityJointInterface> {
     private:
         hardware_interface::JointHandle vel_handle_1;
+        hardware_interface::JointHandle vel_handle_2;
+        hardware_interface::JointHandle vel_handle_3;
+        hardware_interface::JointHandle vel_handle_4;
+        realtime_tools::RealtimeBuffer<geometry_msgs::Twist> cmd_vel_buffer_;
+        WheelSet base;
+        ros::Subscriber cmd_vel_sub;
     public:
         OmniwheelBaseController() {
             ROS_INFO_STREAM("Constructing OmniwheelBaseController");
@@ -18,14 +25,41 @@ class OmniwheelBaseController : public controller_interface::Controller<hardware
         bool init(hardware_interface::VelocityJointInterface* hw, ros::NodeHandle& nh) {
             ROS_INFO_STREAM("Initializing OmniwheelBaseController");
             vel_handle_1 = hw->getHandle("joint1");
-            ROS_INFO_STREAM("Got handle for joint1");
+            vel_handle_2 = hw->getHandle("joint2");
+            vel_handle_3 = hw->getHandle("joint3");
+            vel_handle_4 = hw->getHandle("joint4");
+            ROS_INFO_STREAM("Got handle for joints");
+
+            // Configure base
+            base.addWheel(
+                Wheel(-0.25, -0.25, 45, 0.06, vel_handle_1)
+            );
+            base.addWheel(
+                Wheel(-0.25, 0.25, 315, 0.06, vel_handle_2)
+            );
+            base.addWheel(
+                Wheel(0.25, 0.25, 225, 0.06, vel_handle_3)
+            );
+            base.addWheel(
+                Wheel(0.25, -0.25, 135, 0.06, vel_handle_4)
+            );
+
+            // Subscribe to cmd_vel
+            cmd_vel_sub = nh.subscribe("cmd_vel", 1, &OmniwheelBaseController::cmd_vel_callback, this);
             return true;
         }
+        void cmd_vel_callback(const geometry_msgs::Twist& cmd_vel) {
+            cmd_vel_buffer_.writeFromNonRT(cmd_vel);
+        }
         void update(const ros::Time& time, const ros::Duration& period) {
-            // ROS_INFO_STREAM("Updating OmniwheelBaseController");
+            // Read cmd_vel
+            geometry_msgs::Twist cmd_vel = *(cmd_vel_buffer_.readFromRT());
+            // Calculate wheel velocities
+            base.update(cmd_vel);
         }
         void starting(const ros::Time& time) {
             ROS_INFO_STREAM("Starting OmniwheelBaseController");
+            // Print namespace
         }
         void stopping(const ros::Time& time) {
             ROS_INFO_STREAM("Stopping OmniwheelBaseController");
---
+++ ros/robocock/src/control/omniwheel_base_controller/omniwheel_kinematics.h
@@ -0,0 +1,56 @@
+#include <Eigen/Dense>
+#include <hardware_interface/joint_command_interface.h>
+#include <geometry_msgs/Twist.h>
+#include <iostream>
+
+const Eigen::Matrix2d jacobian = [](){
+    Eigen::Matrix2d jacobian;
+    jacobian << 0, 1, -1, 0;
+    return jacobian;
+}();
+
+class Wheel {
+    public:
+        Wheel(double x, double y, double theta, double radius, hardware_interface::JointHandle& handle) {
+            setProperties(x, y, theta, radius);
+            setHandle(handle);
+        }
+        void setProperties(double x, double y, double theta, double radius) {
+            wheel_position_vec = Eigen::Vector2d(x, y);
+            wheel_direction_vec = Eigen::Vector2d(cos(theta), sin(theta));
+            wheel_radius = radius;
+        }
+        void setHandle(hardware_interface::JointHandle& handle) {
+            wheel_handle = handle;
+        }
+        void update(const geometry_msgs::Twist& twist) {
+            // Getting variables
+            Eigen::Vector2d base_linear_velocity_vec = Eigen::Vector2d(twist.linear.x, twist.linear.y);
+            double base_angular_velocity = twist.angular.z;
+            // Deriving tangential velocity (because of base rotation)
+            Eigen::Vector2d base_tangential_velocity_vec = base_angular_velocity * ( jacobian * wheel_position_vec );
+            // Deriving wheel angular velocity by adding tangential and linear velocity vectors, then projecting onto wheel direction; finally factoring in wheel radius
+            double wheel_angular_velocity = (base_linear_velocity_vec + base_tangential_velocity_vec).dot(wheel_direction_vec) / wheel_radius;
+            // Setting wheel velocity
+            wheel_handle.setCommand(wheel_angular_velocity);
+        }
+    private:
+        Eigen::Vector2d wheel_position_vec = Eigen::Vector2d(0, 0);
+        Eigen::Vector2d wheel_direction_vec = Eigen::Vector2d(1, 0);
+        double wheel_radius = 1;
+        hardware_interface::JointHandle wheel_handle;
+};
+
+class WheelSet {
+    public:
+        void addWheel(const Wheel& wheel) {
+            wheels.push_back(wheel);
+        }
+        void update(const geometry_msgs::Twist& twist) {
+            for (auto& wheel : wheels) {
+                wheel.update(twist);
+            }
+        }
+    private:
+        std::vector<Wheel> wheels;
+};
