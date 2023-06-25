4138301e768099fa872746a14162f896f8bab88b
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-06-25 04:11:27 +0100

    updated messages

--- ros/robocock/src/control/control_parameter_server/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "./control_parameter_server.h"
-
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "control_parameter_server");
-    // Create an instance of ControlParameterServer
-    ControlParameterServer controlServer;
-    ros::spin();
-    return 0;
-}
---
+++ ros/robocock/src/control/hardware_parameter_server/hardware_parameter_server.h
@@ -0,0 +1,62 @@
+#include "ros/ros.h"
+#include "robocock/BaseParameters.h"
+#include "robocock/BaseAdaptiveState.h"
+#include "robocock/BaseState.h"
+
+#define NUM_MOTORS 4
+
+class HardwareParameterServer {
+    private:
+        robocock::BaseParameters base_parameters_msg;
+        robocock::BaseAdaptiveState base_adaptive_state_msg;
+        ros::Publisher base_parameters_pub;
+        ros::Publisher base_adaptive_state_pub;
+        ros::NodeHandle _nh;
+        // Generic type method for initializing a parameter
+        template <typename T>
+        void initParam(T& param, int motor_id, std::string name, T default_value) {
+            _nh.param<T>("motor_" + std::to_string(motor_id + 1) + "/" + name, param, default_value);
+        }
+        bool second_order_predictor_buffer[NUM_MOTORS] = {false};
+        void syncBoolToUint() {
+            for (int i = 0; i < NUM_MOTORS; i++) {
+                base_parameters_msg.parameters[i].second_order_predictor = second_order_predictor_buffer[i];
+            }
+        }
+    public:
+        HardwareParameterServer() : _nh("hardware_parameter_server") {
+            
+            // Initialize the publisher and subscriber
+            base_parameters_pub = _nh.advertise<robocock::BaseParameters>("/base_parameters", 1000);
+            base_adaptive_state_pub = _nh.advertise<robocock::BaseAdaptiveState>("/base_adaptive_state", 1000);
+
+            for (int i = 0; i < NUM_MOTORS; i++) {
+                initParam<float>(base_parameters_msg.parameters[i].p_in, i, "p_in", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].i_in, i, "i_in", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].d_in, i, "d_in", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].bias, i, "bias", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].i_accumulator_min, i, "i_accumulator_min", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].i_accumulator_max, i, "i_accumulator_max", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].output_min, i, "output_min", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].output_max, i, "output_max", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].deadband_min, i, "deadband_min", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].deadband_max, i, "deadband_max", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].target_update_rate, i, "target_update_rate", 30.0);
+                initParam<float>(base_parameters_msg.parameters[i].max_abs_acceleration, i, "max_abs_acceleration", 0.0);
+                initParam<float>(base_parameters_msg.parameters[i].max_abs_velocity, i, "max_abs_velocity", 0.0);
+                initParam<bool>(second_order_predictor_buffer[i], i, "second_order_predictor", false); // Required because the message type is uint and requires conversion
+                initParam<float>(base_parameters_msg.parameters[i].smoothener_alpha, i, "smoothener_alpha", 0.0);    
+
+                initParam<float>(base_adaptive_state_msg.adaptive_states[i].i_accumulator, i, "i_accumulator_initial", 0.0);
+            }
+
+            ROS_INFO("Waiting for /base_state to be published...");
+            ros::topic::waitForMessage<robocock::BaseState>("/base_state");
+            ROS_INFO("Received /base_state message, publishing parameters /base_parameters.");
+
+            // Publish parameters to topics
+            syncBoolToUint();
+            base_parameters_pub.publish(base_parameters_msg);
+            base_adaptive_state_pub.publish(base_adaptive_state_msg);
+        }
+};
