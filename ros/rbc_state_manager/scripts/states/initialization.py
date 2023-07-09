2a04144b9e27f987546c4a1bda0c783bed664536
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-07-10 00:32:14 +0100

    some more research

--- ros/rbc_state_manager/scripts/states/initialization.py
+++ ros/rbc_state_manager/scripts/states/initialization.py
@@ -9,6 +9,19 @@ class Initialization(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('Initializing...')
+
+        """
+        Pseudo code:
+        - Run base bringup
+        - Run lidar bringup
+        - Run scanning camera bringup
+        - Run SLAM bringup (slam_toolbox)
+        - Run navigation bringup (move_base)
+        - If any of the above fails, return 'not_initialized'
+        - If all of the above succeeds, return 'initialized'
+        - Can determine through roslaunch api's .is_alive() and .exit_code [https://github.com/ros/ros_comm/blob/030e132884d613e49a576d4339f0b8ec6f75d2d8/tools/roslaunch/src/roslaunch/nodeprocess.py]
+        """
+
         time.sleep(1)
         if random.random() < 0.1:
             return 'not_initialized'
