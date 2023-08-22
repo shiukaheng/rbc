2d583c82498ccf9fc0a09c9c5c19529e71f704aa
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-08-22 22:39:18 +0800

    Update post_create.sh

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -9,8 +9,7 @@ if [ ! -f ~/.ssh/id_rsa ]; then
 fi
 
 echo -e "\e[36mInitializing dev environment...\e[0m"   
-run_in_directory "catkin build" "$CATKIN_WS_PATH" # Build catkin workspace
-source $CATKIN_WS_PATH/devel/setup.bash # Source catkin workspace
+cb # Catkin build
 abl # Build Arduino libraries
 ac # Compile Arduino sketch
 echo -e "\e[36mDev environment initialized, compiled catkin_ws, arduino rosserial libraries, and arduino sketch.\e[0m"
