aee403938f8ae44ddf9d45c07950f0b296215ab7
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-08-23 19:42:47 +0800

    COMPLETELY REWRITTEN BASHY BASHY STUFF

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,6 +1,3 @@
-source ~/.bashrc
-source ~/convenience.sh
-
 # Generate ssh keys if they don't exist at ~/.ssh/id_rsa
 if [ ! -f ~/.ssh/id_rsa ]; then
     echo -e "\e[36mGenerating ssh keys...\e[0m"
@@ -11,15 +8,5 @@ fi
 # Add ssh keys to ssh-agent
 ssh-add ~/.ssh/id_rsa
 
-echo -e "\e[36mInitializing dev environment...\e[0m"   
-
-# cb # Catkin build
-cd $CATKIN_WS_PATH
-catkin build
-# abl # Build Arduino libraries
-rosrun rosserial_arduino make_libraries.py ~/Arduino/libraries && patch_rosserial_arduino_port
-# ac # Compile Arduino sketch
-cd $RBC_REPO/controller
-arduino-cli compile --fqbn arduino:avr:mega
-
-echo -e "\e[36mDev environment initialized, compiled catkin_ws, arduino rosserial libraries, and arduino sketch.\e[0m"
+# Run acm
+/bin/bash -c "source ~/rbc/.devcontainer/convenience.sh && acm"
