27444e3f3f1018b71cb14d0ad071994676e808e4
ugsfume <86786290+ugsfume@users.noreply.github.com>
2023-09-11 17:56:07 +0800

    updated wheel coordinates&angles

--- ros/rbc_base/src/control/omniwheel_base_controller/omniwheel_base_controller.cpp
+++ ros/rbc_base/src/control/omniwheel_base_controller/omniwheel_base_controller.cpp
@@ -21,19 +21,20 @@ class OmniwheelBaseController : public controller_interface::Controller<hardware
         hardware_interface::JointHandle vel_handle_1;
         hardware_interface::JointHandle vel_handle_2;
         hardware_interface::JointHandle vel_handle_3;
-        hardware_interface::JointHandle vel_handle_4;
+        // hardware_interface::JointHandle vel_handle_4;
         realtime_tools::RealtimeBuffer<geometry_msgs::Twist> cmd_vel_buffer_;
         HolonomicKinematics base;
         ros::Subscriber cmd_vel_sub;
         realtime_tools::RealtimePublisher<nav_msgs::Odometry> odom_pub;
         tf2_ros::TransformBroadcaster tf_broadcaster;
         Pose2D pose;
-        double last_wheel_positions[4] = {0., 0., 0., 0.};
+        // double last_wheel_positions[4] = {0., 0., 0., 0.};
+        double last_wheel_positions[3] = {0., 0., 0.};
         // Placeholders for joint names
         std::string joint_name_1;
         std::string joint_name_2;
         std::string joint_name_3;
-        std::string joint_name_4;
+        // std::string joint_name_4;
     public:
         OmniwheelBaseController() {
         }
@@ -42,7 +43,7 @@ class OmniwheelBaseController : public controller_interface::Controller<hardware
             nh.param<std::string>("joint_name_1", joint_name_1, "wheel_1_joint");
             nh.param<std::string>("joint_name_2", joint_name_2, "wheel_2_joint");
             nh.param<std::string>("joint_name_3", joint_name_3, "wheel_3_joint");
-            nh.param<std::string>("joint_name_4", joint_name_4, "wheel_4_joint");
+            // nh.param<std::string>("joint_name_4", joint_name_4, "wheel_4_joint");
 
             // Log all joint names
             ROS_INFO_STREAM("Joint names: " << joint_name_1 << ", " << joint_name_2 << ", " << joint_name_3 << ", " << joint_name_4);
@@ -51,14 +52,18 @@ class OmniwheelBaseController : public controller_interface::Controller<hardware
             vel_handle_1 = hw->getHandle(joint_name_1);
             vel_handle_2 = hw->getHandle(joint_name_2);
             vel_handle_3 = hw->getHandle(joint_name_3);
-            vel_handle_4 = hw->getHandle(joint_name_4);
+            // vel_handle_4 = hw->getHandle(joint_name_4);
             ROS_INFO_STREAM("Got handle for joints");
 
             // Configure base
-            base.addOmniwheel(0.1575, -0.1575, 45 * M_PI / 180, 0.05);
-            base.addOmniwheel(0.1575, 0.1575, 135 * M_PI / 180, 0.05);
-            base.addOmniwheel(-0.1575, 0.1575, 225 * M_PI / 180, 0.05);
-            base.addOmniwheel(-0.1575, -0.1575, 315 * M_PI / 180, 0.05);
+            // base.addOmniwheel(0.1575, -0.1575, 45 * M_PI / 180, 0.05);
+            // base.addOmniwheel(0.1575, 0.1575, 135 * M_PI / 180, 0.05);
+            // base.addOmniwheel(-0.1575, 0.1575, 225 * M_PI / 180, 0.05);
+            // base.addOmniwheel(-0.1575, -0.1575, 315 * M_PI / 180, 0.05);
+
+            base.addOmniwheel(0.45, 0, 270 * M_PI / 180, 0.05);
+            base.addOmniwheel(-0.225, -0.3897114317, 150 * M_PI / 180, 0.05);
+            base.addOmniwheel(-0.225, 0.3897114317, 30 * M_PI / 180, 0.05);
 
             // Subscribe to cmd_vel
             cmd_vel_sub = nh.subscribe("/cmd_vel", 1, &OmniwheelBaseController::cmd_vel_callback, this);
@@ -78,16 +83,18 @@ class OmniwheelBaseController : public controller_interface::Controller<hardware
             wheel_positions[0] = vel_handle_1.getPosition();
             wheel_positions[1] = vel_handle_2.getPosition();
             wheel_positions[2] = vel_handle_3.getPosition();
-            wheel_positions[3] = vel_handle_4.getPosition();
+            // wheel_positions[3] = vel_handle_4.getPosition();
             
             // Calculate wheel velocities
             Eigen::Vector4d wheel_velocities;
-            for (int i = 0; i < 4; i++) {
+            // for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 3; i++) {
                 wheel_velocities[i] = (wheel_positions[i] - last_wheel_positions[i]) / dt;
             }
 
             // Update last wheel positions
-            for (int i = 0; i < 4; i++) {
+            // for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < 3; i++) {
                 last_wheel_positions[i] = wheel_positions[i];
             }
             return wheel_velocities;
@@ -96,7 +103,7 @@ class OmniwheelBaseController : public controller_interface::Controller<hardware
             vel_handle_1.setCommand(wheel_velocities[0]);
             vel_handle_2.setCommand(wheel_velocities[1]);
             vel_handle_3.setCommand(wheel_velocities[2]);
-            vel_handle_4.setCommand(wheel_velocities[3]);
+            // vel_handle_4.setCommand(wheel_velocities[3]);
         }
         void update(const ros::Time& time, const ros::Duration& period) {
             if (cmd_vel_buffer_.readFromRT()) {
@@ -165,7 +172,7 @@ class OmniwheelBaseController : public controller_interface::Controller<hardware
             vel_handle_1.setCommand(0);
             vel_handle_2.setCommand(0);
             vel_handle_3.setCommand(0);
-            vel_handle_4.setCommand(0);
+            // vel_handle_4.setCommand(0);
         }
 };
 
